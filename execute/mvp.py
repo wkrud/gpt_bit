137d4526fa4415ec9c820d2359d128c9808794e8
wkrud <99162203+wkrud@users.noreply.github.com>
2024-10-09 01:49:19 +0900

    git

--- execute/mvp.py
+++ execute/mvp.py
@@ -79,14 +79,14 @@ def ai_trading():
    ORDER BY HIST_SEQ DESC
    LIMIT 1
   '''
-  result_do = (query_module.fetch_all_data(query))
-  if result_do and len(result_do[0]) > 0:
-    if result_do[0][0] == 'sell':
-      pos_stgy = ['buy', 'hold']
-    else:
-      pos_stgy = ['sell', 'hold']
-  else:
-     print("No result found")
+  # result_do = (query_module.fetch_all_data(query))
+  # if result_do and len(result_do[0]) > 0:
+  #   if result_do[0][0] == 'sell':
+  #     pos_stgy = ['buy', 'hold']
+  #   else:
+  #     pos_stgy = ['sell', 'hold']
+  # else:
+  #    print("No result found")
 
   my_krw = upbit.get_balance("KRW")
   my_btc = upbit.get_balance("KRW-BTC")
@@ -98,11 +98,21 @@ def ai_trading():
       "asset": {
         "KRW": my_krw,
         "BITCOIN": my_btc
-      },
-      "possible_strategy": {
-         "possible_strategy": pos_stgy
       }
   }
+  # strategy = {
+  #     "strategy": {
+  #       "price_data": json_df
+  #     },
+  #     "technical_indicators": df.to_json(),
+  #     "asset": {
+  #       "KRW": my_krw,
+  #       "BITCOIN": my_btc
+  #     },
+  #     "possible_strategy": {
+  #        "possible_strategy": pos_stgy
+  #     }
+  # }
   # print(strategy)
 
   # 2. AI에게 데이터 제공하고 판단 받기
